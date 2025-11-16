ced6b3f5b415b0ee385ab822360e8b81763a65a2
Dingbangzheng <1596664178@qq.com>
2025-11-16 15:12:25 +0800

    Fix syntax errors in launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -4,6 +4,7 @@ int x = 0
 int y,ly,z,lz = 0;
 int main(){
     cout  <<  "The world of character Launcher"  <<  endl;
+    cout  <<  "Checking network connection..."  <<  endl;
     cout  <<  "Checking for updates..."  <<  endl;
     return 0;
 }
