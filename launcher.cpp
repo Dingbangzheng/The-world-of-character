0dfab58086081a0904a9f7386732a2b171c72201
Dingbangzheng <1596664178@qq.com>
2025-11-16 16:55:42 +0800

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -2,14 +2,27 @@
 using namespace std;
 int x = 0
 int y,ly,z,lz = 0;
+#include <cstdlib>
+#include <string>
+#ifdef _WIN32
+    #include <windows.h>
+    #define PING_CMD "ping -n 1 dingbangzheng.cn > nul"
+#else
+    #include <unistd.h>
+    #define PING_CMD "ping -c dingbangzheng.cn > /dev/null 2>&1"
+#endif
+int check_network_connection() {
+    int result = system(PING_CMD);
+    return (result == 0) ? 1 : 0;
+}
 int main(){
     cout  <<  "The world of character Launcher"  <<  endl;
     cout  <<  "Checking network connection..."  <<  endl;
-    cout  <<  "Checking for updates..."  <<  endl;
+    if(check_network_connection()){
+        cout  <<  "Checking for updates..."  <<  endl;
+    }else{
+        cout  <<  "Load and start game.dll..."  <<  endl;
+        //todo
+    }
     return 0;
 }
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    DWORD dwMode = 0;
-    GetConsoleMode(hOut, &dwMode);
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    SetConsoleMode(hOut, dwMode);
