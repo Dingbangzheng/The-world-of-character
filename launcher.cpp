fa9787a9feccc597c87e36701597fb80de2d6409
Dingbangzheng <1596664178@qq.com>
2025-11-28 18:18:25 +0800

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
-using namespace std;
-int x = 0;
-int y = 0;
-int z = 1;
-int ly,lz = 0;
-void cls(){
-    cout  <<  "\033[2J\033[H";
-	return;
-}
 #include <cstdlib>
 #include <string>
+#include <filesystem>
+#include <fstream>
+#include <ctime>
+
 #if defined(_WIN32) || defined(_WIN64)
     #include <windows.h>
     #define PING_CMD "ping -n 1 dingbangzheng.cn > nul"
@@ -17,109 +12,129 @@ void cls(){
     #include <unistd.h>
     #define PING_CMD "ping -c 1 dingbangzheng.cn > /dev/null 2>&1"
 #endif
-#include <filesystem>
-#include <fstream>
-#include <ctime>
+
+int x = 0;
+int y = 0;
+int z = 1;
+int ly, lz = 0;
+
+void cls() {
+    std::cout << "\033[2J\033[H";
+    return;
+}
+
 int check_network_connection() {
-    int result = system(PING_CMD);
+    int result = std::system(PING_CMD);
     return (result == 0) ? 1 : 0;
 }
-int main(){
-    #if defined(_WIN32)  ||  defined(_WIN64)
+
+int main() {
+    #if defined(_WIN32) || defined(_WIN64)
         HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
         DWORD dwMode = 0;
         GetConsoleMode(hOut, &dwMode);
         SetConsoleMode(hOut, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
     #endif
+    
     cls();
-    cout  <<  "The world of character Launcher"  <<  endl;
-    ofstream file("./logs.txt");
-    time_t timestamp = time(nullptr);
-    file <<  timestamp  <<  "-Info[From=launcher]:Game is start."  <<  endl;
+    std::cout << "The world of character Launcher" << std::endl;
+    
+    std::ofstream file("./logs.txt");
+    std::time_t timestamp = std::time(nullptr);
+    file << timestamp << "-Info[From=launcher]:Game is start." << std::endl;
     file.close();
-    cout  <<  "Check network connection..."  <<  endl;
-    if(check_network_connection()){
-        cout  <<  "Pass and check for updates..."  <<  endl;
-        filesystem::remove("./latest_version.txt");
-        string where = string("curl -s -L -O dingbangzheng.cn/twoc/") + to_string(x) + string(".y.z/latest_version.txt");
-        system(where.c_str());
-        if(filesystem::exists("./version.txt")){
-            ifstream file("./version.txt");
-            string version;
-            getline(file,version);
+    
+    std::cout << "Check network connection..." << std::endl;
+    
+    if (check_network_connection()) {
+        std::cout << "Pass and check for updates..." << std::endl;
+        std::filesystem::remove("./latest_version.txt");
+        std::string where = std::string("curl -s -L -O dingbangzheng.cn/twoc/") + std::to_string(x) + std::string(".y.z/latest_version.txt");
+        std::system(where.c_str());
+        
+        if (std::filesystem::exists("./version.txt")) {
+            std::ifstream file("./version.txt");
+            std::string version;
+            std::getline(file, version);
             file.close();
-            sscanf(version.c_str(),".%d.%d",&y,&z);
-        }else{
-            ofstream file("./version.txt");
+            std::sscanf(version.c_str(), ".%d.%d", &y, &z);
+        } else {
+            std::ofstream file("./version.txt");
             file << "." << y << "." << z;
             file.close();
         }
-        if(filesystem::exists("./latest_version.txt")){
-            ifstream file("./latest_version.txt");
-            string latest_version;
-            getline(file,latest_version);
+        
+        if (std::filesystem::exists("./latest_version.txt")) {
+            std::ifstream file("./latest_version.txt");
+            std::string latest_version;
+            std::getline(file, latest_version);
             file.close();
-            sscanf(latest_version.c_str(),".%d.%d",&ly,&lz);
-            filesystem::remove("./latest_version.txt");
-        }else{
-            ofstream file("./logs.txt");
-            timestamp = time(nullptr);
-            file <<  timestamp  <<  "-ERROR[From=launcher,ID=1]:Can not find \"latest_version.txt\" or there is no \"curl\" command."  <<  endl;
+            std::sscanf(latest_version.c_str(), ".%d.%d", &ly, &lz);
+            std::filesystem::remove("./latest_version.txt");
+        } else {
+            std::ofstream file("./logs.txt");
+            timestamp = std::time(nullptr);
+            file << timestamp << "-ERROR[From=launcher,ID=1]:Can not find \"latest_version.txt\" or there is no \"curl\" command." << std::endl;
             file.close();
-            cout << "ERROR[From=launcher,ID=1]:Can not find \"latest_version.txt\" or there is no \"curl\" command."  <<  endl;
+            std::cout << "ERROR[From=launcher,ID=1]:Can not find \"latest_version.txt\" or there is no \"curl\" command." << std::endl;
         }
-        if(ly > y || lz > z){
-            filesystem::remove("./updatedata.txt");
-            cout  <<  "Download updatedata..."  <<  endl;
-            where = string("curl -s -L -O dingbangzheng.cn/twoc/") + to_string(x) + string(".y.z/updatedata.txt");
-            system(where.c_str());
-            if(filesystem::exists("./updatedata.txt")){
-                ifstream file("./updatedata.txt");
-                string name;
-                while(getline(file,name)){
-                    if(!name.empty()  &&  name.back()  ==  '\r'){
+        
+        if (ly > y || lz > z) {
+            std::filesystem::remove("./updatedata.txt");
+            std::cout << "Download updatedata..." << std::endl;
+            where = std::string("curl -s -L -O dingbangzheng.cn/twoc/") + std::to_string(x) + std::string(".y.z/updatedata.txt");
+            std::system(where.c_str());
+            
+            if (std::filesystem::exists("./updatedata.txt")) {
+                std::ifstream file("./updatedata.txt");
+                std::string name;
+                while (std::getline(file, name)) {
+                    if (!name.empty() && name.back() == '\r') {
                         name.pop_back();
                     }
-                    cout  <<  "Remove old \"" <<  name;
+                    std::cout << "Remove old \"" << name;
+                    
                     #if defined(_WIN32) || defined(_WIN64)
-                    cout  <<  ".dll\"."  <<  endl;
-                    filesystem::remove("./data/" + name + ".dll");
-                    cout  <<  "Download and install \""  <<  name  <<  ".dll\"."  <<  endl;
-                    where = string("curl -s -L -o ./data/" + name + ".dll dingbangzheng.cn/twoc/") + to_string(x) + string(".y.z/" + name + ".dll");
-		    system(where.c_str());
+                        std::cout << ".dll\"." << std::endl;
+                        std::filesystem::remove("./data/" + name + ".dll");
+                        std::cout << "Download and install \"" << name << ".dll\"." << std::endl;
+                        where = std::string("curl -s -L -o ./data/" + name + ".dll dingbangzheng.cn/twoc/") + std::to_string(x) + std::string(".y.z/" + name + ".dll");
+                        std::system(where.c_str());
                     #else
-                    cout  <<  ".so\"."  <<  endl;
-                    filesystem::remove("./data/" + name + ".so");
-                    cout  <<  "Download and install \""  <<  name  <<  ".so\"."  <<  endl;
-                    where = string("curl -s -L -o ./data/" + name + ".so dingbangzheng.cn/twoc/") + to_string(x) + string(".y.z/" + name + ".so");
-                    system(where.c_str());
+                        std::cout << ".so\"." << std::endl;
+                        std::filesystem::remove("./data/" + name + ".so");
+                        std::cout << "Download and install \"" << name << ".so\"." << std::endl;
+                        where = std::string("curl -s -L -o ./data/" + name + ".so dingbangzheng.cn/twoc/") + std::to_string(x) + std::string(".y.z/" + name + ".so");
+                        std::system(where.c_str());
                     #endif
                 }
-                filesystem::remove("./updatedata.txt");
+                std::filesystem::remove("./updatedata.txt");
                 y = ly;
                 z = lz;
-                ofstream file2("./version.txt");
-                file2  <<  "."  <<  y  <<  "."  <<  z  <<endl;
+                std::ofstream file2("./version.txt");
+                file2 << "." << y << "." << z << std::endl;
                 file2.close();
-            }else{
-                ofstream file("./logs.txt");
-		timestamp = time(nullptr);
-                file <<  timestamp  <<  "-ERROR[From=launcher,ID=2]:Can not find \"updatedata.txt\" or there is no \"curl\" command."  <<  endl;
+            } else {
+                std::ofstream file("./logs.txt");
+                timestamp = std::time(nullptr);
+                file << timestamp << "-ERROR[From=launcher,ID=2]:Can not find \"updatedata.txt\" or there is no \"curl\" command." << std::endl;
                 file.close();
-                cout << "ERROR[From=launcher,ID=2]:Can not find \"updatedata.txt\" or there is no \"curl\" command."  <<  endl;
+                std::cout << "ERROR[From=launcher,ID=2]:Can not find \"updatedata.txt\" or there is no \"curl\" command." << std::endl;
             }
-        }else{
-            cout  <<  "No updates."  <<  endl;
+        } else {
+            std::cout << "No updates." << std::endl;
         }
-    }else{
-        cout << "Not pass."  <<  endl;
+    } else {
+        std::cout << "Not pass." << std::endl;
     }
+    
     #if defined(_WIN32) || defined(_WIN64)
-        cout  <<  "Load and start game.dll..."  <<  endl;
+        std::cout << "Load and start game.dll..." << std::endl;
         //todo
     #else
-        cout  <<  "Load and start game.so..."  <<  endl;
+        std::cout << "Load and start game.so..." << std::endl;
         //todo
     #endif
+    
     return 0;
 }
