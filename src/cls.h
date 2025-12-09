3930b31a2d8b660274f53eb433c7ad62a0470031
Dingbangzheng <1596664178@qq.com>
2025-12-09 12:23:27 +0000

    update

--- src/cls.h
+++ src/cls.h
@@ -1,12 +1,13 @@
 /***
  * Name:cls.h
- * Version(TWOC):0.0.1
- * Copyleft (C)2025 The TWOC Team*,License under GNU General Public License version 3.0 or later.
- * By:
+ * Version(TWOC*):0.0.1
+ * Copyleft (C)2025 The TWOC Team**,License under GNU General Public License version 3.0 or later.
+ * Authors:
  *     Dingbangzheng <1596664178@qq.com>
  * Introduce:
  *     This is the header file of the TWOC screen clearing function.
- * *The TWOC Team is a non-profit organization.TWOC means The world of character(https://github.com/Dingbangzheng/The-world-of-character).
+ * * TWOC means The world of character(https://github.com/Dingbangzheng/The-world-of-character).
+ * ** The TWOC Team is a non-profit organization.
  ***/
 #include <iostream>
 void cls() {
