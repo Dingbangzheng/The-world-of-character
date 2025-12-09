2b46c92488c5f960fab7e24ae0260d7ff7ae0f22
Dingbangzheng <1596664178@qq.com>
2025-12-09 18:20:29 +0800

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,3 +1,13 @@
+/***
+ * Name:game.cpp(game.dll,game.so)
+ * Version(TWOC):0.0.1
+ * Copyleft (C)2025 The TWOC Team*,License under GNU General Public License version 3.0 or later.
+ * By:
+ *     Dingbangzheng <1596664178@qq.com>
+ * Introduce:
+ *     This is the core function of TWOC.
+ * *The TWOC Team is a non-profit organization.TWOC means The world of character(https://github.com/Dingbangzheng/The-world-of-character).
+ ***/
 #include <iostream>
 #include "cls.h"
 extern "C"{
