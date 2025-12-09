7cbab7bc909949e50d48308831b0b24e47c0a465
Dingbangzheng <1596664178@qq.com>
2025-12-09 10:12:51 +0000

    update

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -1,7 +1,7 @@
 /***
  * Name:launcher.cpp
  * Version(TWOC):0.0.1
- * Copyleft (C)2025 The TWOC Team*,License under GNU General Public License version 3.0 .
+ * Copyleft (C)2025 The TWOC Team*,License under GNU General Public License version 3.0 or later.
  * By:
  *     Dingbangzheng <1596664178@qq.com>
  * Introduce:
