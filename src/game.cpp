779e6c2369a94c6bab252226fbc7861307d5a3be
Dingbangzheng <1596664178@qq.com>
2025-12-10 10:27:04 +0000

    update

--- src/game.cpp
+++ src/game.cpp
@@ -4,8 +4,6 @@
  * Copyleft (C)2025 The TWOC Team**,License under GNU General Public License version 3.0 or later.
  * Authors:
  *     Dingbangzheng <1596664178@qq.com>
- * Introduce:
- *     This is the core function of TWOC.
  * * TWOC means The world of character(https://github.com/Dingbangzheng/The-world-of-character).
  * ** The TWOC Team is a non-profit organization.
  ***/
