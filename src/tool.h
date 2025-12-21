512e100f785e748b86a5d2781c92d7ebebcbfaa6
Dingbangzheng <1596664178@qq.com>
2025-12-21 21:10:54 +0800

    Update tool.h

--- src/tool.h
+++ src/tool.h
@@ -1,6 +1,5 @@
 /***
  * Name:tool.h
- * Version(TWOC*):0.0.1
  * Copyleft (C)2025 The TWOC Team**,License under GNU General Public License version 3.0 or later.
  * Authors:
  *     Dingbangzheng <1596664178@qq.com>
