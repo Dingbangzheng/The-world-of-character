0a9fd9a6382a44f64b3f1e76043b26c2b5e5a23c
Dingbangzheng <1596664178@qq.com>
2025-12-27 13:42:42 +0800

    Update input.h

--- src/input.h
+++ src/input.h
@@ -8,7 +8,6 @@
  ***/
 #ifndef INPUT_H
 #define INPUT_H
-
 #ifdef _WIN32
     #include <conio.h>
     #include <windows.h>
@@ -17,76 +16,69 @@
     #include <unistd.h>
     #include <sys/select.h>
 #endif
-
 class input {
 private:
-#ifdef _WIN32
-    HANDLE hStdin;
-    DWORD oldMode;
-#else
-    termios oldTermios;
-#endif
-
+    #ifdef _WIN32
+        HANDLE hStdin;
+        DWORD oldMode;
+    #else
+        termios oldTermios;
+    #endif
 public:
     input() {
-#ifdef _WIN32
-        hStdin = GetStdHandle(STD_INPUT_HANDLE);
-        GetConsoleMode(hStdin, &oldMode);
-        SetConsoleMode(hStdin, oldMode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT));
-#else
-        tcgetattr(STDIN_FILENO, &oldTermios);
-        termios newTermios = oldTermios;
-        newTermios.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newTermios);
-#endif
+        #ifdef _WIN32
+            hStdin = GetStdHandle(STD_INPUT_HANDLE);
+            GetConsoleMode(hStdin, &oldMode);
+            SetConsoleMode(hStdin, oldMode & ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT));
+        #else
+            tcgetattr(STDIN_FILENO, &oldTermios);
+            termios newTermios = oldTermios;
+            newTermios.c_lflag &= ~(ICANON | ECHO);
+            tcsetattr(STDIN_FILENO, TCSANOW, &newTermios);
+        #endif
     }
-
     ~input() {
-#ifdef _WIN32
-        SetConsoleMode(hStdin, oldMode);
-#else
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldTermios);
-#endif
+        #ifdef _WIN32
+            SetConsoleMode(hStdin, oldMode);
+        #else
+            tcsetattr(STDIN_FILENO, TCSANOW, &oldTermios);
+        #endif
     }
-
     static bool kbhit() {
-#ifdef _WIN32
-        return _kbhit() != 0;
-#else
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(STDIN_FILENO, &readfds);
-        
-        timeval timeout = {0, 0};
-        return select(STDIN_FILENO + 1, &readfds, nullptr, nullptr, &timeout) > 0;
-#endif
+        #ifdef _WIN32
+            return _kbhit() != 0;
+        #else
+            fd_set readfds;
+            FD_ZERO(&readfds);
+            FD_SET(STDIN_FILENO, &readfds);
+            timeval timeout = {0, 0};
+            return select(STDIN_FILENO + 1, &readfds, nullptr, nullptr, &timeout) > 0;
+        #endif
     }
-
     static char getch() {
-#ifdef _WIN32
-        if (_kbhit()) {
-            return _getch();
-        }
-        return 0;
-#else
-        if (kbhit()) {
-            char ch = 0;
-            if (read(STDIN_FILENO, &ch, 1) == 1) {
-                return ch;
+        #ifdef _WIN32
+            if (_kbhit()) {
+                return _getch();
             }
-        }
-        return 0;
-#endif
+            return 0;
+        #else
+            if (kbhit()) {
+                char ch = 0;
+                if (read(STDIN_FILENO, &ch, 1) == 1) {
+                    return ch;
+                }
+            }
+            return 0;
+        #endif
     }
-
     static char getch_wait() {
-#ifdef _WIN32
-        return _getch();
-#else
-        char ch = 0;
-        read(STDIN_FILENO, &ch, 1);
-        return ch;
-#endif
+        #ifdef _WIN32
+            return _getch();
+        #else
+            char ch = 0;
+            read(STDIN_FILENO, &ch, 1);
+            return ch;
+        #endif
     }
 };
 #endif
